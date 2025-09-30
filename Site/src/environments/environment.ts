cfa6505abc990e8565ad61aa03a96e0a15b7ec0e
Logan <logan.janssen@icloud.com>
2025-10-01 08:14:29 +1000

    Removed console log

--- Site/src/environments/environment.ts
+++ Site/src/environments/environment.ts
@@ -3,7 +3,7 @@ import { applyRuntimeOverrides, type EnvironmentConfig } from './environment.run
 const baseEnvironment: EnvironmentConfig = {
   production: false,
   apiBaseUrl: 'http://localhost:3000/api',
-  version: '0.0.1',
+  version: '0.0.2',
 };
 
 export const environment = applyRuntimeOverrides(baseEnvironment);
--- Site/src/main.ts
+++ Site/src/main.ts
@@ -5,17 +5,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
-
-const runtimeOverrides = typeof window !== 'undefined' ? window.__env ?? null : null;
-
-console.info('[FlightDeck] Boot runtime configuration', {
-  production: environment.production,
-  apiBaseUrl: environment.apiBaseUrl,
-  runtimeOverrides,
-});
 
 bootstrapApplication(AppComponent, {
   providers: [
